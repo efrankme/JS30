5da1f7fae8ef5bc56895891767bbc40fc6d5e16a
Frank <molina.frank@gmail.com>
2020-07-10 18:32:22 -0400

    Drum kit

---
+++ drum-kit/sounds.js
@@ -0,0 +1,21 @@
+function playAudio(evt){
+  const audio = document.querySelector(`audio[data-key="${evt.keyCode}"]`);
+
+  if(!audio) return
+
+  const tecla = document.querySelector(`div[data-key="${evt.keyCode}"]`);
+  tecla.classList.add('sonando');
+
+  audio.currentTime = 0;
+  audio.play();
+}
+
+function quitarClaseSonando(evento){
+  if(evento.propertyName != 'transform') return;
+  evento.target.classList.remove('sonando');
+}
+
+const teclas = document.querySelectorAll('.tecla');
+teclas.forEach( tecla => tecla.addEventListener('transitionend',quitarClaseSonando))
+
+document.addEventListener('keydown',playAudio);
